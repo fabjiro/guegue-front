cc7b7adf1587500ac7f6827875ff50411658acc6
fabjiro <fjiron2001@gmail.com>
2024-07-31 21:07:46 -0600

    Se agrego react router y react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <div className='h-screen w-screen'>
-
+      <Routes>
+        <Route element={<div>pagina incial</div>} path="/" />
+      </Routes>
     </div>
   )
 }
